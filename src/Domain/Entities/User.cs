30776006114ca687ed13e36691a4edd4b14e33ed
Juliano Dibai <81191797+julianodibai@users.noreply.github.com>
2022-03-31 21:01:23 -0300

    implementando domain e resolvendo bugs

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,10 +1,56 @@
+using Domain.Validators;
+
 namespace Domain.Entities
 {
     public class User : Base
     {
-        public User()
+        public string Name { get; private set; } 
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+       
+        public User(){} //EF
+        public User(string name, string email, string password)
         {
-            
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+
+            Validate();
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+         public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+        
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+        public override bool Validate()
+        {
+            var validator = new UserValidator(); 
+            var validation = validator.Validate(this); 
+
+            if(!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+                
+
+                throw new Exception($"Alguns campos estão invalidos, por favor corrija-os! {_errors}");
+            }
+
+            return true;
         }
     }
 }
---
+++ src/Domain/Validators/UserValidator.cs
@@ -0,0 +1,63 @@
+using FluentValidation;
+using Domain.Entities;
+
+namespace Domain.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+
+        public UserValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A entidade nao pode ser vazia")
+
+                .NotNull()
+                .WithMessage("A entidade nao pode ser nula");
+            
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("O nome nao pode ser vazia")
+
+                .NotNull()
+                .WithMessage("O nome nao pode ser nulo")
+
+                .MinimumLength(3)
+                .WithMessage("O nome deve ter no minimo 3 caracteres")
+                
+                .MinimumLength(80)
+                .WithMessage("O nome deve ter no maximo 80 caracteres");
+        
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("A senha nao pode ser vazia")
+
+                .NotNull()
+                .WithMessage("A senha nao pode ser nula")
+
+                .MinimumLength(6)
+                .WithMessage("A senha deve ter no minimo 6 caracteres")
+                
+                .MinimumLength(80)
+                .WithMessage("A senha deve ter no maximo 80 caracteres")
+                
+                .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$")
+                .WithMessage("Deve conter ao menos um dígito, deve conter ao menos uma letra minúscula, deve conter ao menos uma letra maiúscula");
+            
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("O email nao pode ser vazio")
+
+                .NotNull()
+                .WithMessage("O email nao pode ser nulo")
+
+                .MinimumLength(10)
+                .WithMessage("O email deve ter no minimo 10 caracteres")
+                .MinimumLength(180)
+                .WithMessage("O email deve ter no maximo 180 caracteres")
+                
+                .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+                .WithMessage("O email informado não é válido.");
+        }
+    }
+}
